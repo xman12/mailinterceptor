9d157fee9639e8a7427b799922439fc0786b5f37
Aleksandr Belyshev <xman12@mail.ru>
2022-03-16 00:28:05 +0600

    clear blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,24 +3,18 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="current-uid" content="@current_uid(request())" />
-    <meta name="recaptcha-site-key" content="{{ env('RECAPTCHA_SITE_KEY') }}" />
-    @stack('meta')
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&display=swap&subset=cyrillic-ext" rel="stylesheet">
 
     <title>Главная</title>
     <meta name="description" lang="ru" content="@yield('description')" />
-    {{--    <script src="{{ mix('assets/vendor.dll.js') }}"></script>--}}
-    @stack('styles')
 </head>
 <body>
 
     @php
     /**
-    * @var \src\DTO\MailDTO[] $mails
+    * @var \MailInterceptor\DTO\MailDTO[] $mails
     */
     @endphp
     @foreach($mails as $key => $mail)
