acc5b5091c1e3d108966edf78d3259b76e6371a1
Aleksandr Belyshev <xman12@mail.ru>
2022-03-16 00:24:19 +0600

    update views

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,23 @@
-@extends('layouts.base')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="current-uid" content="@current_uid(request())" />
+    <meta name="recaptcha-site-key" content="{{ env('RECAPTCHA_SITE_KEY') }}" />
+    @stack('meta')
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700&display=swap&subset=cyrillic-ext" rel="stylesheet">
 
-@section('title')Главная@endsection
+    <title>Главная</title>
+    <meta name="description" lang="ru" content="@yield('description')" />
+    {{--    <script src="{{ mix('assets/vendor.dll.js') }}"></script>--}}
+    @stack('styles')
+</head>
+<body>
 
-@section('content')
     @php
     /**
     * @var \src\DTO\MailDTO[] $mails
@@ -46,4 +61,6 @@
         }
 
     </style>
-@endsection
+
+</body>
+</html>
