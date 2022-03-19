069f1208a05ac3acf0c241c46cc54f683e24dd6d
Aleksandr Belyshev <xman12@mail.ru>
2022-03-19 11:44:35 +0600

    replace code

--- src/Providers/AppMailInterceptorServiceProvider.php
+++ src/Providers/AppMailInterceptorServiceProvider.php
@@ -30,40 +30,6 @@ class AppMailInterceptorServiceProvider extends ServiceProvider
             ), $app['config']['app.asset_url']
             );
         });
-
-//        $this->app->bind('url', MailInterceptorUrlGenerator::class);
-//        $this->app->singleton('url', function ($app) {
-//            /** @var \Illuminate\Foundation\Application $app */
-//            $routes = $app['router']->getRoutes();
-//
-//            $app->instance('routes', $routes);
-//
-//            // *** THIS IS THE MAIN DIFFERENCE ***
-//            $url = new MailInterceptorUrlGenerator(
-//                $routes,
-//                $app->rebinding(
-//                    'request',
-//                    static function ($app, $request) {
-//                        $app['url']->setRequest($request);
-//                    }
-//                ),
-//                $app['config']['app.asset_url']
-//            );
-//
-//            $url->setSessionResolver(function () {
-//                return $this->app['session'] ?? null;
-//            });
-//
-//            $url->setKeyResolver(function () {
-//                return $this->app->make('config')->get('app.key');
-//            });
-//
-//            $app->rebinding('routes', static function ($app, $routes) {
-//                $app['url']->setRoutes($routes);
-//            });
-//
-//            return $url;
-//        });
     }
 
     protected function requestRebinder()
