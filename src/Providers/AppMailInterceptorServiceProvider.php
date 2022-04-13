dff0bd38d5c198c4660cb9e05a4414e474529a05
Aleksandr Belyshev <xman12@mail.ru>
2022-04-13 12:07:40 +0600

    refactoring registration custom url generator

--- src/Providers/AppMailInterceptorServiceProvider.php
+++ src/Providers/AppMailInterceptorServiceProvider.php
@@ -22,15 +22,8 @@ class AppMailInterceptorServiceProvider extends ServiceProvider
         $this->app->singleton(MailLogServiceInterface::class, function ($app) {
             return new MailLogService($app);
         });
-
-        $this->app->singleton("url", function ($app) {
-            $routes = $app['router']->getRoutes();
-            return new MailInterceptorUrlGenerator(  // this is actually my class due to the namespace above
-                $routes, $app->rebinding(
-                'request', $this->requestRebinder()
-            ), $app['config']['app.asset_url']
-            );
-        });
+        
+        $this->registerUrlGenerator();
     }
 
     /**
@@ -46,5 +39,43 @@ class AppMailInterceptorServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    protected function registerUrlGenerator()
+    {
+        $this->app->singleton('url', function ($app) {
+            $routes = $app['router']->getRoutes();
+
+            // The URL generator needs the route collection that exists on the router.
+            // Keep in mind this is an object, so we're passing by references here
+            // and all the registered routes will be available to the generator.
+            $app->instance('routes', $routes);
+
+            $url = new MailInterceptorUrlGenerator(
+                $routes, $app->rebinding(
+                'request', $this->requestRebinder()
+            ), $app['config']['app.asset_url']
+            );
+
+            // Next we will set a few service resolvers on the URL generator so it can
+            // get the information it needs to function. This just provides some of
+            // the convenience features to this URL generator like "signed" URLs.
+            $url->setSessionResolver(function () {
+                return $this->app['session'] ?? null;
+            });
+
+            $url->setKeyResolver(function () {
+                return $this->app->make('config')->get('app.key');
+            });
+
+            // If the route collection is "rebound", for example, when the routes stay
+            // cached for the application, we will need to rebind the routes on the
+            // URL generator instance so it has the latest version of the routes.
+            $app->rebinding('routes', function ($app, $routes) {
+                $app['url']->setRoutes($routes);
+            });
+
+            return $url;
+        });
     }
 }
